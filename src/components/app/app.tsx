f378c59458b70d018954b70867e8fc26f454e9c8
Polina Tutygina <polina.tutygina@yandex.ru>
2025-06-30 17:02:54 +0300

    Добавлены базовые маршруты: /, /feed и страница 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+
 import { ConstructorPage } from '@pages';
+import { Feed } from '@pages';
+import { NotFound404 } from '@pages';
+
 import '../../index.css';
 import styles from './app.module.css';
 
@@ -6,8 +11,14 @@ import { AppHeader } from '@components';
 
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <Router>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </Router>
   </div>
 );
 
